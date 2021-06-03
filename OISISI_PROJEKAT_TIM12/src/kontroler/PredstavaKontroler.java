d4bcff4b72e99d3532703ae71d55800a9cc4ffbd
Milos <83866633+MilosNestorovic99@users.noreply.github.com>
2021-06-03 16:27:51 +0200

    Update PredstavaKontroler.java

--- OISISI_PROJEKAT_TIM12/src/kontroler/PredstavaKontroler.java
+++ OISISI_PROJEKAT_TIM12/src/kontroler/PredstavaKontroler.java
@@ -1,12 +1,14 @@
 package kontroler;
 
 import aplikacija.Singleton;
+import komponente.ProzorPredstavaDetaljanPrikaz;
 import komponente.ProzorPredstave;
 import komponente.Ruter;
 import model.Predstava;
+import model.Pretraga;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PredstavaKontroler {
 
@@ -18,11 +20,66 @@ public class PredstavaKontroler {
 
     public void kreirajPredstavu(Predstava predstava) {
         Set<Long> kljucevi = this.predstave.keySet();
-        Long sifra = new Long(kljucevi.size()+1);
+        Long sifra = (long)(kljucevi.size()+1);
         predstava.setSifra(sifra);
         this.predstave.put(sifra, predstava);
         Ruter ruter = Singleton.getInstance().getRuter();
+        ruter.osveziProzor(ProzorPredstave.class);
         ruter.promeniProzor(ProzorPredstave.class);
         System.out.println("\nKreirana predstava: " + predstava);
+    }
+
+    public void prikaziDetaljno(Predstava predstava) {
+        Singleton.getInstance().setDetaljnoPrikazanaPredstava(predstava);
+        Ruter ruter = Singleton.getInstance().getRuter();
+        ruter.osveziProzor(ProzorPredstavaDetaljanPrikaz.class);
+        ruter.promeniProzor(ProzorPredstavaDetaljanPrikaz.class);
+    }
+
+
+
+    public List<Predstava> pretraziPredstave(Pretraga pretraga) {
+        Set<Predstava> rezultat = new HashSet<>();
+        Set<Predstava> setNaziv = new HashSet<>();
+        Set<Predstava> setCena = new HashSet<>();
+        Set<Predstava> setDatum = new HashSet<>();
+        boolean filterNaziv = false;
+        boolean filterCena = false;
+        boolean filterDatum = false;
+
+        if (!pretraga.getNaziv().equals("")) {
+            filterNaziv = true;
+            this.predstave.values().forEach(predstava -> {
+                if (predstava.getNaziv().toLowerCase().contains(pretraga.getNaziv().toLowerCase()))
+                    setNaziv.add(predstava);
+            });
+        }
+        if (pretraga.getGornjaCena() > pretraga.getDonjaCena()) {
+            filterCena = true;
+            this.predstave.values().forEach(predstava -> {
+                if (predstava.getCena() < pretraga.getGornjaCena() && predstava.getCena() > pretraga.getDonjaCena())
+                    setCena.add(predstava);
+            });
+        }
+        if (pretraga.getKrajnjiDatum().after(pretraga.getPocetniDatum())) {
+            filterDatum = true;
+            this.predstave.values().forEach(predstava -> {
+                if (predstava.getDatumVreme().after(pretraga.getPocetniDatum()) &&
+                        pretraga.getKrajnjiDatum().after(predstava.getDatumVreme()))
+                    setDatum.add(predstava);
+            });
+        }
+
+        rezultat.addAll(setNaziv);
+        rezultat.addAll(setCena);
+        rezultat.addAll(setDatum);
+        if (filterNaziv)
+            rezultat.retainAll(setNaziv);
+        if (filterCena)
+            rezultat.retainAll(setCena);
+        if (filterDatum)
+            rezultat.retainAll(setDatum);
+
+        return rezultat.stream().collect(Collectors.toList());
     }
 }
